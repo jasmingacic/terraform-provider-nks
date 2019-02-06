8e1942922bc6a6e0f595cd256b86b72012a2d4b8
Amel Ajdinović <amel.a.z@gmail.com>
2019-02-06 01:19:58 +0100

    NKS-959 - Updated GCE example.

--- examples/gce/single-master-two-workers.tf
+++ examples/gce/single-master-two-workers.tf
@@ -4,36 +4,58 @@ provider "nks" {
      defaults to StackPointCloud production enviroment */
 }
 
-data "nks_keysets" "keyset_default" {
+data "nks_organization" "default" {
+  name = "${var.organization_name}"
+}
+
+data "nks_keyset" "keyset_default" {
+  org_id = "${data.nks_organization.default.id}"
+  name = "${var.provider_keyset_name}"
+  category = "provider"
+  entity   = "${var.provider_code}"
+
+  /* You can specify a custom orgID here,   
+     or the system will find and use your default organization ID */
+}
+
+data "nks_keyset" "keyset_ssh" {
+  org_id = "${data.nks_organization.default.id}"
+  category = "user_ssh"
+  name     = "${var.ssh_keyset_name}"
+
   /* You can specify a custom orgID here,   
      or the system will find and use your default organization ID */
 }
 
 data "nks_instance_specs" "master-specs" {
-  provider_code = "${var.gce_code}"
-  node_size     = "${var.gce_master_size}"
+  provider_code = "${var.provider_code}"
+  node_size     = "${var.provider_master_size}"
 }
 
 data "nks_instance_specs" "worker-specs" {
-  provider_code = "${var.gce_code}"
-  node_size     = "${var.gce_worker_size}"
+  provider_code = "${var.provider_code}"
+  node_size     = "${var.provider_worker_size}"
 }
 
 resource "nks_cluster" "terraform-cluster" {
-  org_id               = "${data.nks_keysets.keyset_default.org_id}"
-  cluster_name         = "Test GCE Cluster TerraForm2"
-  provider_code        = "${var.gce_code}"
-  provider_keyset      = "${data.nks_keysets.keyset_default.gce_keyset}"
-  region               = "${var.gce_region}"
-  k8s_version          = "${var.gce_k8s_version}"
-  startup_master_size  = "${data.nks_instance_specs.master-specs.node_size}"
-  startup_worker_count = 2
-  startup_worker_size  = "${data.nks_instance_specs.worker-specs.node_size}"
-  region               = "${var.gce_region}"
-  rbac_enabled         = true
-  dashboard_enabled    = true
-  etcd_type            = "classic"
-  platform             = "${var.gce_platform}"
-  channel              = "stable"
-  ssh_keyset           = "${data.nks_keysets.keyset_default.user_ssh_keyset}"
+  org_id                          = "${data.nks_organization.default.id}"
+  cluster_name                    = "${var.cluster_name}"
+  provider_code                   = "${var.provider_code}"
+  provider_keyset                 = "${data.nks_keyset.keyset_default.id}"
+  region                          = "${var.provider_region}"
+  k8s_version                     = "${var.provider_k8s_version}"
+  startup_master_size             = "${data.nks_instance_specs.master-specs.node_size}"
+  startup_worker_count            = 2
+  startup_worker_size             = "${data.nks_instance_specs.worker-specs.node_size}"
+  region                          = "${var.provider_region}"
+  rbac_enabled                    = true
+  dashboard_enabled               = true
+  etcd_type                       = "${var.provider_etcd_type}"
+  platform                        = "${var.provider_platform}"
+  channel                         = "${var.provider_channel}"
+  ssh_keyset                      = "${data.nks_keyset.keyset_ssh.id}"
+  provider_network_id_requested   = "${var.provider_network_id}"
+  provider_network_cidr           = "${var.provider_network_cidr}"
+  provider_subnet_id_requested    = "${var.provider_subnet_id}"
+  provider_network_cidr           = "${var.provider_subnet_cidr}"
 }
--- examples/gce/variables.tf
+++ examples/gce/variables.tf
@@ -1,34 +1,83 @@
-variable "gce_code" {
-  description = "GCE cluster type code"
-  default     = "gce"
+# Organization
+variable "organization_name" {
+  description = "NKS organization name"
+  default     = ""
 }
 
-variable "gce_k8s_version" {
-  description = "GCE kubernetes version"
-  default     = "v1.8.3"
+# Cluster
+variable "cluster_name" {
+  description = "NKS cluster name"
+  default     = ""
+} 
+
+# Keyset
+variable "ssh_keyset_name" {
+  description = "NKS ssh keyset name"
+  default     = ""
 }
 
-variable "gce_platform" {
-  description = "GCE platform type"
-  default     = "coreos"
+variable "provider_keyset_name" {
+  description = "Cloud provider keyset name"
+  default     = ""
 }
 
-variable "gce_region" {
-  description = "GCE region"
-  default     = "us-west1-a"
+# Cloud provider configuration variables
+variable "provider_code" {
+  description = "Cloud provider type code"
+  default     = ""
 }
 
-variable "gce_master_size" {
-  description = "GCE master node size"
-  default     = "n1-standard-1"
+variable "provider_k8s_version" {
+  description = "Cloud provider kubernetes version"
+  default     = ""
 }
 
-variable "gce_worker_size" {
-  description = "GCE worker node size"
-  default     = "n1-standard-1"
+variable "provider_etcd_type" {
+  description = "Cloud provider etcd type"
+  default     = ""
 }
 
-variable "gce_region2" {
-  description = "GCE region"
-  default     = "us-west1-b"
+variable "provider_channel" {
+  description = "Cloud provider channel"
+  default     = ""
+}
+
+variable "provider_platform" {
+  description = "Cloud provider platform type"
+  default     = ""
+}
+
+variable "provider_region" {
+  description = "Cloud provider region"
+  default     = ""
+}
+
+variable "provider_network_id" {
+  description = "Cloud provider network ID"
+  default     = ""
+}
+
+variable "provider_network_cidr" {
+  description = "Cloud provider network CIDR"
+  default     = ""
+}
+
+variable "provider_subnet_id" {
+  description = "Cloud provider subnet ID"
+  default     = ""
+}
+
+variable "provider_subnet_cidr" {
+  description = "Cloud provider subnet CIDR"
+  default     = ""
+}
+
+variable "provider_master_size" {
+  description = "Cloud provider master node size"
+  default     = ""
+}
+
+variable "provider_worker_size" {
+  description = "Cloud provider worker node size"
+  default     = ""
 }
